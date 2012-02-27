53831230b814fe2a9eb0a857591f880a3d8f68e6
Baptiste Fontaine <batifon@yahoo.fr>
2012-02-27 13:30:53 +0100

    KataRomanNumerals : tests fixed, simple cases implemented

--- KataRomanNumerals.sh
+++ KataRomanNumerals.sh
@@ -1,10 +1,29 @@
 #! /bin/bash
 
+ROMAN_N=(
+            [1]=I
+            [5]=V
+           [10]=X
+           [50]=L
+          [100]=C
+          [500]=D
+         [1000]=M
+        );
+
 # Part I
 dec_to_roman() {
+    # simple cases
+    if [ ${ROMAN_N[$1]} ]; then echo ${ROMAN_N[$1]}; fi;
+
+    # others
+    d=$1;
+    r=;
+
+    echo $r;
 }
 
 # Part II
 roman_to_dec() {
+    echo "";
 }
 
--- KataRomanNumerals_test.sh
+++ KataRomanNumerals_test.sh
@@ -4,35 +4,35 @@
 . assert.sh;
 
 # Part I
-should_returns_I()      { return dec_to_roman    1; }
-should_returns_VII()    { return dec_to_roman    8; }
-should_returns_IX()     { return dec_to_roman    9; }
-should_returns_XV()     { return dec_to_roman   15; }
-should_returns_XIX()    { return dec_to_roman   19; }
-should_returns_XLI()    { return dec_to_roman   41; }
-should_returns_L()      { return dec_to_roman   50; }
-should_returns_XCIX()   { return dec_to_roman   99; }
-should_returns_C()      { return dec_to_roman  100; }
-should_returns_CC()     { return dec_to_roman  200; }
-should_returns_D()      { return dec_to_roman  500; }
-should_returns_DXCIX()  { return dec_to_roman  599; }
-should_returns_DCCXLIX(){ return dec_to_roman  749; }
-should_returns_M()      { return dec_to_roman 1000; }
-should_returns_MCMXC()  { return dec_to_roman 1990; }
-should_returns_MMVIII() { return dec_to_roman 2008; }
+should_returns_I()      { echo $(dec_to_roman    1); }
+should_returns_VII()    { echo $(dec_to_roman    8); }
+should_returns_IX()     { echo $(dec_to_roman    9); }
+should_returns_XV()     { echo $(dec_to_roman   15); }
+should_returns_XIX()    { echo $(dec_to_roman   19); }
+should_returns_XLI()    { echo $(dec_to_roman   41); }
+should_returns_L()      { echo $(dec_to_roman   50); }
+should_returns_XCIX()   { echo $(dec_to_roman   99); }
+should_returns_C()      { echo $(dec_to_roman  100); }
+should_returns_CC()     { echo $(dec_to_roman  200); }
+should_returns_D()      { echo $(dec_to_roman  500); }
+should_returns_DXCIX()  { echo $(dec_to_roman  599); }
+should_returns_DCCXLIX(){ echo $(dec_to_roman  749); }
+should_returns_M()      { echo $(dec_to_roman 1000); }
+should_returns_MCMXC()  { echo $(dec_to_roman 1990); }
+should_returns_MMVIII() { echo $(dec_to_roman 2008); }
 
 # Part II
-should_returns_1()    { return roman_to_dec "I"; }
-should_returns_4()    { return roman_to_dec "IV"; }
-should_returns_7()    { return roman_to_dec "VII"; }
-should_returns_10()   { return roman_to_dec "X"; }
-should_returns_50()   { return roman_to_dec "L"; }
-should_returns_100()  { return roman_to_dec "C"; }
-should_returns_500()  { return roman_to_dec "D"; }
-should_returns_501()  { return roman_to_dec "DI"; }
-should_returns_1000() { return roman_to_dec "M"; }
-should_returns_1991() { return roman_to_dec "MCMXCI"; }
-should_returns_2007() { return roman_to_dec "MMVII"; }
+should_returns_1()    { echo $(roman_to_dec "I"); }
+should_returns_4()    { echo $(roman_to_dec "IV"); }
+should_returns_7()    { echo $(roman_to_dec "VII"); }
+should_returns_10()   { echo $(roman_to_dec "X"); }
+should_returns_50()   { echo $(roman_to_dec "L"); }
+should_returns_100()  { echo $(roman_to_dec "C"); }
+should_returns_500()  { echo $(roman_to_dec "D"); }
+should_returns_501()  { echo $(roman_to_dec "DI"); }
+should_returns_1000() { echo $(roman_to_dec "M"); }
+should_returns_1991() { echo $(roman_to_dec "MCMXCI"); }
+should_returns_2007() { echo $(roman_to_dec "MMVII"); }
 
 # -- tests ---------
 
