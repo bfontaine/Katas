4046e7eaa42b24aed3c8a721a0af9c7124ec1f6a
Baptiste Fontaine <batifon@yahoo.fr>
2012-02-28 12:42:37 +0100

    KataRomanNumerals: simple cases for roman_to_dec partially implemented

--- KataRomanNumerals.sh
+++ KataRomanNumerals.sh
@@ -16,14 +16,30 @@ dec_to_roman() {
     if [ ${ROMAN_N[$1]} ]; then echo ${ROMAN_N[$1]}; fi;
 
     # others
-    d=$1;
-    r=;
+    d=$1; # decimal
+    r=; #   roman
 
     echo $r;
+    return 0;
 }
 
 # Part II
 roman_to_dec() {
-    echo "";
+    # simple cases
+    for i in ${!a[@]};
+    do
+        if [ ${a[$i]} == $1 ];
+        then
+            echo $i;
+            return 0;
+        fi
+    done
+
+    # others
+    r=$1; # roman
+    d=; #   decimal
+
+    echo $d;
+    return 0;
 }
 
