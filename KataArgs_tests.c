a31c343e701743ce6567b028a92a0f162c7abf55
Baptiste Fontaine <batifon@yahoo.fr>
2012-03-28 18:12:56 +0200

    KataArgs_tests.c now reads schema file from command-line (must be first argument)

--- KataArgs_tests.c
+++ KataArgs_tests.c
@@ -1,6 +1,11 @@
 #include "KataArgs.c"
+#include <string.h>
 
 int main(int argc, char** argv) {
+
+    if (argc == 0) {
+        return -1;
+    }
 
     int flag1 = 0,
         flag2 = 0;
@@ -16,7 +21,13 @@ int main(int argc, char** argv) {
 #endif
 
     arg* config = NULL;
-    int config_length = read_config_file_f(SCHEMA_FILE, config, flag1);
+
+    /* the first argument is the schema file */
+    char* schema_file = strdup(argv[0]);
+    argv++;
+    argc--;
+
+    int config_length = read_config_file_f(schema_file, config, flag1);
 
     read_args_f(argc, argv, config, config_length, flag2);
 
