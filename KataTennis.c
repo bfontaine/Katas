0dccea7ae1e555cfbc7df3084ca24f7bd7c8e1e7
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-04 12:18:41 +0200

    KataTennis: init functions implemented

--- KataTennis.c
+++ KataTennis.c
@@ -1,10 +1,25 @@
 #include "KataTennis.h"
 
-void new_turn(player* p1, player* p2) {}
+void new_turn(player* p1, player* p2) {
+    p1->game_score = 0;
+    p1->has_advantage = 0;
 
-void new_tennis(player* p1, player* p2) {}
+    p2->game_score = 0;
+    p2->has_advantage = 0;
+}
 
-void beat(player* p1, player* p2) {}
+
+void new_tennis(player* p1, player* p2) {
+    p1->score = 0;
+    p2->score = 0;
+    new_turn(p1, p2);
+}
+
+void beat(player* p1, player* p2) {
+
+
+
+}
 
 char* compare_scores(player* p1, player* p2) {
     
