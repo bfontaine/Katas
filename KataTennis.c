f84fcc9b979383141b1d6f3d02290caace172a6a
Baptiste Fontaine <batifon@yahoo.fr>
2012-04-04 12:15:17 +0200

    KataTennis one test passed

--- KataTennis.c
+++ KataTennis.c
@@ -6,4 +6,11 @@ void new_tennis(player* p1, player* p2) {}
 
 void beat(player* p1, player* p2) {}
 
-char* compare_scores(player* p1, player* p2) {return "";}
+char* compare_scores(player* p1, player* p2) {
+    
+    if ((p1->game_score == 0) && (p2->game_score == 0)) {
+        return LOVE;
+    }
+
+    return "";
+}
