3259f55667a2128cc7defeb28faabb978f58e45d
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-02 16:29:37 +0200

    function to convert indices to square names added

--- KataReversi.lisp
+++ KataReversi.lisp
@@ -7,6 +7,10 @@
 
 (in-package #:kata-reversi)
 
+(defvar *letters* (coerce "ABCDEFGH" 'list))
+
+(defun get-square (i j)
+  (format nil "~a~a" (nth i *letters*) (+ 1 j)))
 
 (defun possible-moves(tab)
   NIL ;TODO
