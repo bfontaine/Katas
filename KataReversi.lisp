76501b1efe5c9cd5df6e15c8441d4596db4c3c55
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-03 00:02:53 +0200

    comments added, + 2 useful functions

--- KataReversi.lisp
+++ KataReversi.lisp
@@ -9,9 +9,24 @@
 
 (defvar *letters* (coerce "ABCDEFGH" 'list))
 
+;;; Returns the square {i,j}'s name
 (defun get-square (i j)
   (format nil "~a~a" (nth i *letters*) (+ 1 j)))
 
-(defun possible-moves(tab)
+;;; Returns the character at the given position
+;;; in a list representing a Reversi board, lines are separated by
+;;; one character.
+(defun at (x y tab)
+  (nth (+ (* (+ 1 (list-length *letters)) y) x) tab))
+
+;;; Returns the char of the current player
+(defun get-player (tab)
+  (first (last tab)))
+
+;;; Returns a list of possible moves for the current player
+(defun possible-moves(s)
+  (let (tab) (coerce s 'list))
+  (let (player) (get-player tab))
+
   NIL ;TODO
 )
