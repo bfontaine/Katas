7f0bc508b090c73e2420223364d0c69942195c61
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-03 22:54:13 +0200

    function 'chop' implemented

--- KataKarateChop.lisp
+++ KataKarateChop.lisp
@@ -7,5 +7,14 @@
 
 (in-package #:kata-karate-chop)
 
-(defun chop (i tab)
-  )
+;;; Returns the indice of n in tab if it exists, else -1. l should be
+;;; the length of tab.
+(defun _chop (n tab l)
+  (if (eq nil tab)
+      (- -1 l)
+      (if (eq n (car tab))
+          0
+          (+ 1 (_chop n (cdr tab) l)))))
+
+;;; Returns the indice of n in tab if it exists, else -1
+(defun chop (n tab) (_chop n tab (list-length tab)))
