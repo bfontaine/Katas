0ff5c1236a6bf54004f93378032726a1707dadf2
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-07 13:34:36 +0200

    'require' tests added

--- KataReversi_tests.pl
+++ KataReversi_tests.pl
@@ -4,3 +4,11 @@ use warnings;
 use strict;
 
 use Test::More 'no_plan';
+
+require_ok 'KataReversi.pm';
+require_ok 'KataReversi';
+use_ok 'KataReversi';
+
+use KataReversi;
+
+ok( defined &KataReversi::get_moves, "get_moves function is defined." )
