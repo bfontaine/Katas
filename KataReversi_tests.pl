34a6d382124d772c9a185fd6ddc7e13d826ac67c
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-07 14:08:51 +0200

    initial board test added

--- KataReversi_tests.pl
+++ KataReversi_tests.pl
@@ -11,4 +11,25 @@ use_ok 'KataReversi';
 
 use KataReversi;
 
-ok( defined &KataReversi::get_moves, "get_moves function is defined." )
+ok( defined &KataReversi::get_moves, "get_moves function is defined." );
+
+is( KataReversi::get_moves, undef, "With no given value, get_moves returns an undef value.");
+
+my $init_test = <<EOT
+........
+........
+........
+...BW...
+...WB...
+........
+........
+........
+B
+EOT
+;
+
+isa_ok ( KataReversi::get_moves($init_test), 'ARRAY', "With good given value, get_moves(…) returns an array." );
+
+is ( KataReversi::get_moves($init_test), ('C5', 'D6', 'E3', 'F4'),
+            "with the initial board, Black player can play in C5/D6/E3/F4");
+
