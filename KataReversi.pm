76448df27ff49ca973c3ac060d343c4c2961350b
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-07 16:43:38 +0200

    get_moves returns undef with bad input

--- KataReversi.pm
+++ KataReversi.pm
@@ -3,7 +3,10 @@
 package KataReversi;
 
 sub get_moves {
+    my $input = shift or return undef;
+    return undef if ($input !~ m/^([\.BW]{8}\n){8}[BW]\n?$/);
 
+    return [42];
 }
 
 # return a true value
