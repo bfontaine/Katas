12135dd40215145e4e4e75114c783ce925ad5687
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-08 13:08:24 +0200

    2 tests added

--- KataGameOfLife_tests.groovy
+++ KataGameOfLife_tests.groovy
@@ -3,7 +3,12 @@ import static org.junit.Assert.assertEquals
 
 class KataGameOfLife_tests {
 
-    static empty_gen = "Generation 1:\n4 8\n........\n........\n........\n........"
+    static empty_gen              = "Generation 1:\n4 8\n........\n........\n........\n........"
+    
+    static one_cell_gen           = "Generation 1:\n4 8\n........\n....*...\n........\n........"
+
+    static topleft_corner_gen     = "Generation 1:\n4 8\n*.......\n........\n........\n........"
+    static bottomright_corner_gen = "Generation 1:\n4 8\n........\n........\n........\n......**"
 
     @Test
     void parse_unparse_empty_gen() {
@@ -13,5 +18,15 @@ class KataGameOfLife_tests {
     @Test
     void empty_next_gen() {
         assertEquals(empty_gen, KataGameOfLife.parse(empty_gen).nextGen().toString());
+    }
+
+    @Test
+    void one_cell_next_gen() {
+        assertEquals(empty_gen, KataGameOfLife.parse(one_cell_gen).nextGen().toString());
+    }
+
+    @Test
+    void top_left_corner_next_gen() {
+        assertEquals(empty_gen, KataGameOfLife.parse(topleft_corner_gen).nextGen().toString());
     }
 }
