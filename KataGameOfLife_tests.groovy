6f8536cd82ecbbdc9354e665e542403672474117
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-08 12:32:44 +0200

    test for parse/unparse added

--- KataGameOfLife_tests.groovy
+++ KataGameOfLife_tests.groovy
@@ -3,4 +3,10 @@ import static org.junit.Assert.assertEquals
 
 class KataGameOfLife_tests {
 
+    static empty_gen = "Generation 1:\n4 8\n........\n........\n........\n........"
+
+    @Test
+    void parse_unparse_empty_gen() {
+        assertEquals(empty_gen, KataGameOfLife.parse(empty_gen).toString());
+    }
 }
