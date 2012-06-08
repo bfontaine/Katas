9434337191d9fa189d82c614423b8148936403fd
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-08 13:34:01 +0200

    tests for known patterns added

--- KataGameOfLife_tests.groovy
+++ KataGameOfLife_tests.groovy
@@ -18,6 +18,25 @@ class KataGameOfLife_tests {
     static high_gen               = ["Generation 578:\n4 8\n........\n........\n........\n........",
                                      "Generation 579:\n4 8\n........\n........\n........\n........"]
 
+    // patterns below taken from
+    // http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Examples_of_patterns
+
+    // "still lifes"
+
+    static block_gen              = ["Generation 1:\n4 4\n....\n.**.\n.**.\n....",
+                                     "Generation 2:\n4 4\n....\n.**.\n.**.\n....",
+                                     "Generation 3:\n4 4\n....\n.**.\n.**.\n...."]
+
+    static beehive_gen            = ["Generation 1:\n5 6\n......\n..**..\n.*..*.\n..**..\n......",
+                                     "Generation 2:\n5 6\n......\n..**..\n.*..*.\n..**..\n......",
+                                     "Generation 3:\n5 6\n......\n..**..\n.*..*.\n..**..\n......"]
+
+    // "oscillators"
+
+    static blinker_gen            = ["Generation 1:\n5 5\n.....\n.....\n.***.\n.....\n.....",
+                                     "Generation 2:\n5 5\n.....\n..*..\n..*..\n..*..\n.....",
+                                     "Generation 3:\n5 5\n.....\n.....\n.***.\n.....\n....."]
+
     @Test
     void parse_unparse_empty_gen() {
         assertEquals(empty_gen[0],
@@ -46,5 +65,32 @@ class KataGameOfLife_tests {
     void bottom_right_corner_next_gen() {
         assertEquals(bottomright_corner_gen[1],
                         KataGameOfLife.parse(bottomright_corner_gen[0]).nextGen().toString());
+    }
+
+    @Test
+    void block_next_gen() {
+        assertEquals(block_gen[1],
+                        KataGameOfLife.parse(block_gen[0]).nextGen().toString());
+
+        assertEquals(block_gen[2],
+                        KataGameOfLife.parse(block_gen[0]).nextGen().nextGen().toString());
+    }
+
+    @Test
+    void beehive_next_gen() {
+        assertEquals(beehive_gen[1],
+                        KataGameOfLife.parse(beehive_gen[0]).nextGen().toString());
+
+        assertEquals(beehive_gen[2],
+                        KataGameOfLife.parse(beehive_gen[0]).nextGen().nextGen().toString());
+    }
+
+    @Test
+    void blinker_next_gen() {
+        assertEquals(blinker_gen[1],
+                        KataGameOfLife.parse(blinker_gen[0]).nextGen().toString());
+
+        assertEquals(blinker_gen[2],
+                        KataGameOfLife.parse(blinker_gen[0]).nextGen().nextGen().toString());
     }
 }
