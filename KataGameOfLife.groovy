5ff077a80865659b671699cceb16354a4a30d6c5
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-08 13:08:39 +0200

    nextGen() method implemented

--- KataGameOfLife.groovy
+++ KataGameOfLife.groovy
@@ -13,19 +13,66 @@ class KataGameOfLife {
 class Generation {
     def genNum
     def board
+    def cols
+    def lines
 
-    Generation(Integer gn, List b) {
-       this.genNum = gn;
-       this.board = b;
+    Generation(int gn, List b) {
+       this.genNum = gn
+       this.board = b
+
+       this.lines = b.size()
+       this.cols = (lines == 0) ? 0 : b[0].length()
+    }
+
+    private getNbNeighbours(int i, int j) {
+        def n = 0
+        
+        for (int x = i-1; x <= i+1; x++) {
+            for (int y = j-1; y <= j-1; y++) {
+                if ((x == i) && (y == j))
+                    continue
+
+                if (this.board[x] == null)
+                    continue
+
+                n += (this.board[x][y] && this.board[x][y] == "*") ? 1 : 0
+            }
+        }
+        n
     }
 
     Generation nextGen() {
-        this;
+
+        def newBoard = []
+        def line
+        def cell
+
+        for (int i=0; i<this.lines; i++) {
+            
+            line = ""
+            for (int j=0; j<this.cols; j++) {
+            
+                cell = "."
+
+                def nb = this.getNbNeighbours(i, j)
+
+                if (((this.board[i][j] == ".") && (nb == 3)) \
+                    || ((this.board[i][j] == "*") && ((nb == 2) || (nb == 3)))) {
+                    cell = "*"
+                }
+
+                line += cell
+            }
+            
+            newBoard.push(line)
+        }
+
+        new Generation(this.genNum, newBoard)
     }
 
     String toString() {
          "Generation ${this.genNum}:\n" \
-        +"${this.board.size()} ${this.board[0].length()}\n" \
+        +"${this.lines} ${this.cols}\n" \
         +"${this.board.join("\n")}"
     }
 }
