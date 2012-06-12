c7138bd9668103df0411a475de9c817c29d28dfc
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-13 00:32:01 +0200

    wordwrap partially implemented (not working)

--- KataWordWrap.cpp
+++ KataWordWrap.cpp
@@ -2,6 +2,36 @@
 using namespace std;
 
 string Wrapper::wrap(const string *s, int cols) {
-    return *s;
+
+    if ((s->length() == 0) || (cols <= 1)) {
+        return *s;
+    }
+
+    unsigned int i = 0;
+    int last_space = -1;
+    string s2 = *s;
+
+    const string newline = "\n";
+
+    /* FIXME */
+    for (; i < s2.length(); i++) {
+        if (i%cols == 0) {
+            if (::isspace(s2[i])) {
+                s2[i] = '\n';
+            } else if (last_space == -1) {
+                return s2;
+            } else {
+                s2.insert(last_space, newline);
+            }
+            last_space = -1;
+        }
+        else if (::isspace(s2[i])) {
+            last_space = i;
+        }
+
+        
+    }
+
+    return s2;
 }
 
