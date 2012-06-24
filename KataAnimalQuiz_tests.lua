f6a963651cbe31ef56131ef798242c9a0573fd2a
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-24 23:37:28 +0200

    init function tests added

--- KataAnimalQuiz_tests.lua
+++ KataAnimalQuiz_tests.lua
@@ -1,2 +1,33 @@
 #! /usr/bin/lua
 
+local quiz = require 'KataAnimalQuiz'
+local lunit = require 'lunitx'
+
+_ENV = lunit.module('enhanced','seeall')
+
+function setup()
+    quiz.init()
+end
+
+function test_init_nil()
+    local animals = quiz.getAnimals()
+
+    assert_table(animals)
+    assert_equal(0, #animals)
+end
+
+function test_init_number()
+    quiz.init(42)
+    local animals = quiz.getAnimals()
+
+    assert_table(animals)
+    assert_equal(0, #animals)
+end
+
+function test_init_ok()
+    quiz.init('dog')
+    local animals = quiz.getAnimals()
+
+    assert_table(animals)
+    assert_equal('dog', animals[1])
+end
