fd16b4cf4a1fbfcdc38d79dc56ab0e11ea41a02d
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-24 23:41:01 +0200

    TODOs added

--- KataAnimalQuiz.lua
+++ KataAnimalQuiz.lua
@@ -12,4 +12,18 @@ function quiz.getAnimals()
     return animals
 end
 
+--[[
+    TODO:
+        - add a .load() and .dump() functions to load/dump "database" (animals)
+          into a file
+        - use a tree structure to store animals, e.g.:
+                            Q1
+                          y/  \n
+                          A1   Q2
+                             y/ \n
+                             A2  A3
+          
+          with Qx = questions, Ax = animals (y: yes, n: no)
+]]
+
 return quiz
