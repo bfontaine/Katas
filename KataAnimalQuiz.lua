e8a93bf0c6cf8dff1e75708de8ca9ba9c49dda79
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-24 23:37:40 +0200

    init function added

--- KataAnimalQuiz.lua
+++ KataAnimalQuiz.lua
@@ -1,2 +1,15 @@
 #! /usr/bin/lua
 
+local quiz = {}
+
+local animals = {}
+
+function quiz.init(animal)
+    if (type(animal) == 'string') then animals = { animal } end
+end
+
+function quiz.getAnimals()
+    return animals
+end
+
+return quiz
