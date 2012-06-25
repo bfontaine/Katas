10e2eacbfe0b277d3e3070f20b109527aafdabcb
Baptiste Fontaine <batifon@yahoo.fr>
2012-06-25 16:42:27 +0200

    using objects to represent Animals/Questions

--- KataAnimalQuiz.lua
+++ KataAnimalQuiz.lua
@@ -1,21 +1,80 @@
 #! /usr/bin/lua
 
-local quiz = {}
+-- Animal ---------------------
 
-local animals = {}
+local Animal = {}
+Animal.__index = Animal
 
-function quiz.init(animal)
-    if (type(animal) == 'string') then animals = { animal } end
+function Animal.create(name)
+    local a = {}
+    setmetatable(a, Animal)
+    a.name = name
+    a.is_a = 'Animal'
+    return a
 end
 
-function quiz.getAnimals()
-    return animals
+-------------------------------
+-- Question -------------------
+
+local Question = {}
+Question.__index = Question
+
+function Question.create(s, y_resp, n_resp)
+    local q = {}
+    setmetatable(q, Question)
+    q.string = s
+    q.y_resp = y_resp
+    q.n_resp = n_resp
+    q.is_a = 'Question'
+    return q
 end
+
+-------------------------------
+-- Node -----------------------
+
+local Node = {}
+Node.__index = Node
+
+function Node.create(value)
+    local n = {}
+    setmetatable(n, Node)
+    n.value = value
+    return n
+end
+
+-- change current value into a question
+function Node:Make_question(q_string, y_resp, n_resp)
+   self.value = Question.create(q_string, y_resp, n_resp) 
+end
+
+-------------------------------
+-- Quiz -----------------------
+
+local Quiz = {}
+Quiz.__index = Quiz
+
+function Quiz.create(root)
+    local q = {}
+    local n
+    setmetatable(q, Quiz)
+    if (type(root) == 'string') then
+        n = Node.create(Animal.create(root))
+    else if (type(root_node) == 'table') then
+            n = Node.create(root)
+         else
+             return nil
+         end
+    end
+    q.root = n
+    return q
+end
+
+-------------------------------
 
 --[[
     TODO:
         - add a .load() and .dump() functions to load/dump "database" (animals)
-          into a file
+          into a file (just for fun)
         - use a tree structure to store animals, e.g.:
                             Q1
                           y/  \n
@@ -26,4 +85,4 @@ end
           with Qx = questions, Ax = animals (y: yes, n: no)
 ]]
 
-return quiz
+return Quiz
