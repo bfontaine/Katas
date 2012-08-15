4062ee5f0bdeee7091986509de36dc0b7ce07341
Baptiste Fontaine <batifon@yahoo.fr>
2012-08-15 14:19:38 +0200

    refactoring of 'checkout' function

--- katacheckout_tests.erl
+++ katacheckout_tests.erl
@@ -6,7 +6,26 @@ empty_test () ->
     ?assertEqual(0, katacheckout:checkout([], [{apple, 2}], [])).
 
 free_test () ->
-    ?assertEqual(0, katacheckout:checkout([apple, apple], [], [])),
-    ?assertEqual(0, katacheckout:checkout([apple, apple], [{apple, 0}], [])),
-    ?assertEqual(0, katacheckout:checkout([apple, apple, banana], [{banana, 0}], [])).
+    ?assertEqual(0,
+        katacheckout:checkout([apple, apple], [], [])),
+    ?assertEqual(0,
+        katacheckout:checkout([apple, apple], [{apple, 0}], [])),
+    ?assertEqual(0,
+        katacheckout:checkout([apple, apple, banana], [{banana, 0}], [])),
+    ?assertEqual(0,
+        katacheckout:checkout([apple, banana, apple, peach],
+            [{banana, 0}, {apple, 0}, {peach, 0}], [])).
 
+no_promotion_test () ->
+    (?assertEqual(42,
+        katacheckout:checkout([apple, apple], [{apple, 21}], []))),
+    (?assertEqual(42,
+        katacheckout:checkout([apple, apple], [{banana, 2}, {apple, 21}], []))),
+    (?assertEqual(17,
+        katacheckout:checkout([apple, banana, apple],
+            [{banana, 7}, {apple, 5}], []))),
+    (?assertEqual(3.5,
+        katacheckout:checkout([apple, banana, peach],
+            [{banana, 1}, {apple, 1}, {peach, 1.5}], []))),
+    (?assertEqual(1,
+        katacheckout:checkout([apple], [{apple, 1}], [{apple, 3, 5}]))).
