ca8e6dd6ceb6231c169253932d210b24f249b0fa
Baptiste Fontaine <batifon@yahoo.fr>
2012-09-16 10:20:53 +0200

    some tests added

--- katadependencies_test.go
+++ katadependencies_test.go
@@ -44,7 +44,7 @@ func Test_NoDependencies(t *testing.T) {
     if len(A_dependencies) != 0 { t.Error() }
 }
 
-func Test_OneDependencies(t *testing.T) {
+func Test_OneDependency(t *testing.T) {
     D := New_Dependencies()
     D.add_direct("A", []string{"B"})
 
@@ -52,4 +52,42 @@ func Test_OneDependencies(t *testing.T) {
 
     if len(A_dependencies) != 1 { t.Error() }
     if A_dependencies[0]   != "B" { t.Error() }
+}
+
+func Test_TwoDependencies(t *testing.T) {
+    D := New_Dependencies()
+    D.add_direct("A", []string{"B", "C"})
+
+    A_dependencies := D.dependencies_for("A")
+
+    if len(A_dependencies) != 2 { t.Error() }
+    if A_dependencies[0]   != "B" { t.Error() }
+    if A_dependencies[1]   != "C" { t.Error() }
+}
+
+func Test_CascadingDependencies(t *testing.T) {
+    D := New_Dependencies()
+    D.add_direct("A", []string{"B"})
+    D.add_direct("B", []string{"C"})
+
+    A_dependencies := D.dependencies_for("A")
+
+    if len(A_dependencies) != 2 { t.Error() }
+    if A_dependencies[0]   != "B" { t.Error() }
+    if A_dependencies[1]   != "C" { t.Error() }
+}
+
+func Test_CircularDependencies(t *testing.T) {
+    D := New_Dependencies()
+    D.add_direct("A", []string{"B"})
+    D.add_direct("B", []string{"C", "E"})
+    D.add_direct("C", []string{"B", "D"})
+
+    A_dependencies := D.dependencies_for("A")
+
+    if len(A_dependencies) != 4 { t.Error() }
+    if A_dependencies[0]   != "B" { t.Error() }
+    if A_dependencies[1]   != "C" { t.Error() }
+    if A_dependencies[2]   != "E" { t.Error() }
+    if A_dependencies[3]   != "D" { t.Error() }
 }
