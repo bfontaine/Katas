8ba89b91074bc5baddee5b10db92e94f2a09fd5e
Baptiste Fontaine <batifon@yahoo.fr>
2013-05-18 18:42:32 +0200

    'index' action implemented ; it stores trigrams in JSON.

--- KataTrigrams/project.clj
+++ KataTrigrams/project.clj
@@ -1,6 +1,7 @@
 (defproject kata-trigrams "1.0.0-SNAPSHOT"
   :description "Random text generator using trigrams."
   :url "https://github.com/bfontaine/Katas"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/data.json "0.2.2"]]
   :main kata-trigrams.cli
   :repl-init kata-trigrams.core)
--- KataTrigrams/src/kata_trigrams/core.clj
+++ KataTrigrams/src/kata_trigrams/core.clj
@@ -1,11 +1,39 @@
 #! /usr/bin/clojure
-(ns kata-trigrams.core)
+(ns kata-trigrams.core
+  (:require [clojure.data.json :as json])
+  (:use [clojure.string :only (split)]))
+
+(defn- str->trigrams
+  "Return trigrams from a string. It splits on spaces, so punctuation
+   is kept."
+  [s]
+  (let [words (split s #"\s+")]
+    (partition 3 1 words)))
+
+(defn- add-trigram
+  "Add a trigram to a map."
+  ;; the map maps words pairs ("w1 w2") to sets of possible third words, e.g.:
+  ;; { "foo bar" #{"baz" "quux" ... }, "bar foo" #{...}, ... }
+  [m t]
+  (let [k (str (first t) " " (second t)) ; key
+        v (last t)]                      ; value
+    (if (m k)
+      ;; if the key exists, append the value to its set
+      (assoc m k (conj (m k) v))
+      ;; if not, create its set
+      (assoc m k #{v}))))
 
 (defn index
-  "Index all trigrams from filename-in and store them in filename-out."
+  "Index all trigrams from filename-in and store them in filename-out
+   (JSON)."
+  ;; JSON Format:
+  ;;   { "word1 word2": ["word3-1", "word3-2", "word3-3", ...], ... }
+  ;;
+  ;; We keep punctuation, too, and treat newlines as whitespaces.
   [filename-in filename-out]
-  ;; TODO
-  )
+  (let [trigrams (str->trigrams (slurp filename-in))]
+    (spit filename-out
+          (json/write-str (reduce add-trigram {} trigrams)))))
 
 (defn generate
   "Generate a random text from trigrams stored in index-filename with
