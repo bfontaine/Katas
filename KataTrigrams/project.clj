6c454b7969c377931023903e8e29544a9a0d4c01
Baptiste Fontaine <batifon@yahoo.fr>
2013-05-20 01:16:12 +0200

    Including clojure/core.incubator for -?> macro.

--- KataTrigrams/project.clj
+++ KataTrigrams/project.clj
@@ -2,6 +2,7 @@
   :description "Random text generator using trigrams."
   :url "https://github.com/bfontaine/Katas"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/data.json "0.2.2"]]
+                 [org.clojure/data.json "0.2.2"]
+                 [org.clojure/core.incubator "0.1.2"]]
   :main kata-trigrams.cli
   :repl-init kata-trigrams.core)
