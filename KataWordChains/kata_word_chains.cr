09890623ba8b322a1d3a3b182cc17c0a202cde2a
Baptiste Fontaine <batifon@yahoo.fr>
2014-09-07 22:57:48 +0200

    KataWordChains almost done

--- KataWordChains/kata_word_chains.cr
+++ KataWordChains/kata_word_chains.cr
@@ -16,7 +16,11 @@ class Dict
     end
   end
 
-  def initialize(@words)
+  def initialize(words)
+    @words = Set.new(words)
+  end
+
+  def initialize(@words : Set)
   end
 
   # return a new dictionnary without one word
@@ -33,76 +37,96 @@ class Dict
   end
 end
 
-def differ_by_one_letter?(word1, word2)
-  return false if word1 == word2
-  diff = 0
-  # String#each_char_with_index should be available in Crystal 0.4.4
-  i, max = 0, word1.length
-  while i < max
-    c1, c2 = word1[i], word2[i]
-    i += 1
-    next if c1 == c2
-    diff += 1
-    return false if diff > 1
+class Chain
+
+  getter :words
+
+  def initialize(startword : String, @dict : Dict)
+    @words = [startword]
   end
-  true
+
+  def initialize(@words, @dict)
+  end
+
+  # return all possible chains from this one
+  def successors
+    last = @words.last
+    chs = KataWordChains.successors(last, @dict)
+    dict = @dict.pop(last)
+
+    chs.map do |word|
+      words = self.words.clone.push(word)
+      Chain.new(words, dict)
+    end
+  end
 end
 
-# return all possible successors for a word (`ref`). Those are words from a
-# dictionnary (`dict`) that differ from the reference word by only one letter.
-def successors(ref, dict)
-  dict.words.select do |word|
-    differ_by_one_letter?(ref, word)
+module KataWordChains
+  extend self
+
+  # pre-condition: both words are of the same length
+  def differ_by_one_letter?(word1, word2)
+    return false if word1 == word2
+    diff = 0
+    # String#each_char_with_index should be available in Crystal 0.4.4
+    i, max = 0, word1.length
+    while i < max
+      c1, c2 = word1[i], word2[i]
+      i += 1
+      next if c1 == c2
+      diff += 1
+      return false if diff > 1
+    end
+    true
   end
-end
 
-# return a words chain between word1 and word2
-def chain(word1, word2, dict)
-  chs = Set.new([[word1]])
+  # return all possible successors for a word (`ref`). Those are words from a
+  # dictionnary (`dict`) that differ from the reference word by only one letter.
+  def successors(ref, dict)
+    dict.words.select do |word|
+      differ_by_one_letter?(ref, word)
+    end
+  end
 
-  possible = true
+  def chain(word1, word2, dict, maxlen=10)
+    chs = [Chain.new(word1, dict)]
 
-  # TODO class Chain, a list of words (the chain) and its current dictionnary
-  # since we have to maintain one dictionnary per chain
-  while possible
-    chs.each do |ch|
-      last = ch.last
-
-      return ch if last == word2
-
-      dict = dict.pop(last)
-      possible = false
-      next if dict.empty?
-      possible = true
-
-      chs.delete ch
-
-      successors(last, dict).each do |succ|
-        chs << ch.clone.push(succ)
+    until chs.empty?
+      chs2 = [] of Chain
+      chs.each do |ch|
+        ch.successors.each do |succ|
+          # good chain
+          return succ.words if succ.words.last == word2
+          # too long, stopping
+          next if succ.words.length >= maxlen
+          # continue to search
+          chs2 << succ
+        end
       end
+
+      chs = chs2
     end
   end
 
-  nil
-end
-
-# run the program with both words
-def run(word1, word2)
-  len = word1.length
-  if word2.length != len
-    puts "Error: Both words should have the same length."
-    return 1
-  end
-  english = Dict.new("english", len)
-  result = chain(word1, word2, english)
-  if result
-    result.each do |word|
-      puts word
+  # run the program with both words
+  def run(word1, word2)
+    len = word1.length
+    if word2.length != len
+      puts "Error: Both words should have the same length."
+      return 1
     end
-  else
-    puts "No chain between '#{word1}' and '#{word2}'."
+    english = Dict.new("english", len)
+    result = chain(word1, word2, english)
+    if result
+      result.each do |word|
+        puts word
+      end
+    else
+      puts "No chain between '#{word1}' and '#{word2}'."
+    end
+    0
   end
-  0
+
 end
 
 unless $TESTS
@@ -114,5 +138,5 @@ unless $TESTS
   end
 
   word1, word2 = ARGV
-  exit run(word1, word2)
+  exit KataWordChains.run(word1, word2)
 end
